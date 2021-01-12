5d011f51ac9c7467d51ce945c7a73ad34b353e95
vzdrizhni <vzdrizhni@gmail.com>
2021-01-12 18:53:38 +0500

    Create tow controllers with json response

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const feedRoutes = require('./routes/feed')
 
 const app = express();
+
+app.use(bodyParser.json())
 
 app.use('/feed', feedRoutes);
 
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,20 @@
 exports.getPosts = (req, res, next) => {
-    
+  res
+    .status(200)
+    .json({message: 'First Post'})
+}
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  res
+    .status(201)
+    .json({
+      message: 'Post created',
+      post: {
+        id: new Date().toISOString(),
+        title: title,
+        content: content
+      }
+    })
 }
