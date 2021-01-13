6aebee63edad51ebf7536749e988698e19284a65
vzdrizhni <vzdrizhni@gmail.com>
2021-01-13 14:26:59 +0500

    Allow cross origin resource sharing

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const feedRoutes = require('./routes/feed')
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Headers', 'Content-Type', 'Authorization');
+    next();
+})
 
 app.use('/feed', feedRoutes);
 
