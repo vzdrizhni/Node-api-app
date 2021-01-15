3a9f3a52bb48828db96c141178fded493ea08b76
vzdrizhni <vzdrizhni@gmail.com>
2021-01-15 16:14:20 +0500

    Add picture upload.

--- app.js
+++ app.js
@@ -2,12 +2,31 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const multer = require('multer');
 
 const feedRoutes = require('./routes/feed')
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images')
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname)
+  }
+})
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg') {
+    cb(null, true)
+  } else {
+    cb(null, false)
+  }
+}
+
 app.use(bodyParser.json());
+app.use(multer({storage: fileStorage, filter: fileFilter}).single('image'))
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
