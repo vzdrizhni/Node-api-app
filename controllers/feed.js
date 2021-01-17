7b1d6f96444ea4c25e2b6c34c7a3a009beecb994
vzdrizhni <vzdrizhni@gmail.com>
2021-01-17 18:08:08 +0500

    Add post deletion

--- controllers/feed.js
+++ controllers/feed.js
@@ -119,6 +119,29 @@ exports.updatePost = (req, res, next) => {
   })
 }
 
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+      .then(post => {
+        if (!post) {
+          const error = new Error('Could not find post');
+          error.statusCode = 404;
+          throw error;
+        }
+        clearImage(post.imageUrl);
+        Post.findByIdAndRemove(postId);
+      })
+      .then(result => {
+        res.status(200).json({message: 'Post deleted'})
+      })
+      .catch(err => {
+        if (!err.statusCode) {
+          err.statusCode = 500;
+        }
+        next(err);
+      })
+}
+
 const clearImage = (filePath) => {
   filePath = path.join(__dirname, '..', filePath);
   fs.unlink(filePath, err => {
--- routes/feed.js
+++ routes/feed.js
@@ -17,6 +17,8 @@ router.get('/post/:postId', feedController.getPost);
 router.put('/post/:postId', [
     body('title').trim().isLength({min: 5}),
     body('content').trim().isLength({min: 5})
-], feedController.updatePost)
+], feedController.updatePost);
+
+router.delete('/post/:postId', feedController.deletePost)
 
 module.exports = router
