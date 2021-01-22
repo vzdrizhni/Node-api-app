167bbdabccc2edc603385647f34f8acb1bd35458
vzdrizhni <vzdrizhni@gmail.com>
2021-01-22 20:02:49 +0500

    Add user login

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -32,5 +33,22 @@ module.exports = {
     });
     const createdUser = await user.save();
     return { ...createdUser._doc, _id: createdUser._id.toString() };
+  },
+
+  login: async ({email, password}) => {
+    const user = await User.findOne({email: email});
+    if (!user) {
+      const error = new Error('User not found');
+      error.code = 401;
+      throw error;
+    }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      const error = new Error('Password doesnt match');
+      error.code = 401;
+      throw error;
+    }
+    const token = jwt.sign({userId: user._id.toString(), email: email}, 'somesupersecret', {expiresIn: '1h'});
+    return {token: token, userId: user._id.toString()}
   }
 };
