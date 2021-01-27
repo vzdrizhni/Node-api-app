1cad5a61c3a14537d3ffcebe4b496c3eedeb7c04
vzdrizhni <vzdrizhni@gmail.com>
2021-01-27 21:30:20 +0500

    Add file upload

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -24,11 +25,7 @@ const fileStorage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
-  ) {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
     cb(null, true);
   } else {
     cb(null, false);
@@ -39,17 +36,12 @@ app.use(cors());
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
-app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
-);
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-  );
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') {
     return res.status(200)
@@ -58,6 +50,22 @@ app.use((req, res, next) => {
 });
 
 app.use(auth);
+
+app.put('/post-image', (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error('Not authenticated')
+  }
+  if (!req.file) {
+    res
+      .status(200)
+      .json({message: 'No image provided'})
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath)
+  }
+  return res.status(200).json({message: 'Nice!', filePath: req.file.path})
+})
+
 
 app.use('/graphql', graphqlHTTP({
   schema: graphQlSchema,
@@ -79,18 +87,24 @@ app.use((error, req, res, next) => {
   const status = error.statusCode || 500;
   const message = error.message;
   const data = error.data;
-  res.status(status).json({ message: message, data: data });
+  res
+    .status(status)
+    .json({message: message, data: data});
 });
 
 mongoose
-  .connect(
-    'mongodb+srv://vzd:1@cluster0.8frdi.mongodb.net/messages',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
-  )
+  .connect('mongodb+srv://vzd:1@cluster0.8frdi.mongodb.net/messages', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(result => {
     app.listen(8080);
   })
   .catch(err => console.log(err));
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, err => {
+    console.log(err)
+  })
+}
